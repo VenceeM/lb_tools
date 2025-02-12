be8e25faf6325941e75a933e8851d00794fcd5c2
officialvencee <officialvencee.manansala@gmail.com>
2025-02-12 13:19:52 +0800

    Added password hashing and jwt tokens utils

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,11 @@ from pydantic_settings import BaseSettings,SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_HOST:str
-    
+    PASSWORD_SECRET:str
+    JWT_SECRET:str
+    JWT_ALORITHM:str
+    REDIS_HOST:str
+    REDIS_PORT:str
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore"
