fc55f4593f082f4f6e0d6244ebf1523d63ea0e35
officialvencee <officialvencee.manansala@gmail.com>
2025-02-12 23:35:36 +0800

    Protect role routes

--- app/api/v1/role/main.py
+++ app/api/v1/role/main.py
@@ -6,18 +6,21 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from fastapi.responses import JSONResponse
 from typing import List
 from app.db.db import get_session
+from app.dependency.dependencies import AccessTokenBearer,RoleChecker
 
 role_routes = APIRouter()
 role_service = RoleService()
 
+admin_role = Depends(RoleChecker(["admin"]))
 
-@role_routes.get("/",response_model=List[RoleModel])
+
+@role_routes.get("/",response_model=List[RoleModel],dependencies=[admin_role])
 async def get_all_roles(session:AsyncSession = Depends(get_session)):
     roles = await role_service.get_roles(session)
     
     return roles
 
-@role_routes.post("/",response_model=RoleModel)
+@role_routes.post("/",response_model=RoleModel,dependencies=[admin_role])
 async def create(role_data:RoleSchema, session:AsyncSession = Depends(get_session)):
     new_role = await role_service.create_role(
         role_data=role_data,
@@ -26,7 +29,7 @@ async def create(role_data:RoleSchema, session:AsyncSession = Depends(get_sessio
     
     return new_role
 
-@role_routes.patch("/{uid}",response_model=RoleModel)
+@role_routes.patch("/{uid}",response_model=RoleModel, dependencies=[admin_role])
 async def update(uid:str,role_data:RoleSchema, session:AsyncSession = Depends(get_session)):
     updated_role = await role_service.update_role(
         uid=uid,
