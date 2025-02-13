18453d9078cb0b2bcb5a6c8dec744185e5339990
officialvencee <officialvencee.manansala@gmail.com>
2025-02-13 14:06:22 +0800

    add role to extraction

--- app/api/v1/extract/extract.py
+++ app/api/v1/extract/extract.py
@@ -2,15 +2,18 @@ from fastapi import APIRouter,HTTPException,Depends,status
 from app.db.db import get_other_engine_session
 from sqlmodel.ext.asyncio.session import AsyncSession
 from app.core.helper import Helper
+from app.dependency.dependencies import AccessTokenBearer,RoleChecker
 
 extract_route = APIRouter()
 helper = Helper()
+access_token_bearer = AccessTokenBearer()
+role_checker = Depends(RoleChecker(["admin"]))
 
-@extract_route.get("/")
-async def extract_weekly_data(session:AsyncSession = Depends(get_other_engine_session)):
+@extract_route.get("/", dependencies=[role_checker])
+async def extract_weekly_data(session:AsyncSession = Depends(get_other_engine_session), token_details = Depends(access_token_bearer)):
     result = await helper.extract(session=session)
     return result
     
-@extract_route.post("/")
-async def send_extraction(to:str,subject:str,body:str):
+@extract_route.post("/", dependencies=[role_checker])
+async def send_extraction(to:str,subject:str,body:str,token_details = Depends(access_token_bearer)):
     return helper.send_email(recipient_email=to,subject=subject,body=body)
